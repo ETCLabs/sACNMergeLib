dcfeb869571074613f3e481881f1654fcb75ae03
creising <creising@gmail.com>
2019-05-21 00:05:53 -0400

    IE11 fix

--- src/calculate.js
+++ src/calculate.js
@@ -12,13 +12,13 @@
  * @param {TestObj[]} input an array of input objects
  * @returns {number[]} an array of numbers
  */
-const calculate = (input) => {
+function calculate(input) {
   if (input === undefined || input === null) {
     return [];
   }
 
   const rtn = [];
-  input.forEach((obj) => {
+  input.forEach(function(obj) {
     rtn.push(obj.num + 2);
   });
   return rtn;
