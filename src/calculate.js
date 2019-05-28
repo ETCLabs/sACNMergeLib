886a20b5e1d4ab6f591d7dfca20a97f2c3a4fd2c
Josh Tveite <Josh.Tveite@etcconnect.com>
2019-05-28 18:16:46 -0500

    Add initial merge function and some tests. These tests are currently mostly failing because the implementation is not real yet.

--- src/calculate.js
+++ src/calculate.js
@@ -7,6 +7,74 @@
  */
 
 /**
+* 
+* @typedef SACNUniverse
+* @type {Object}
+* @property {number[]} values The values of this universe for this source.
+* @property {number} priority The universe priority for this source.
+* @property {number[]} [perAddressPriority]  The priority of each address in this universe.
+*  
+*/
+
+/**
+ * Merges two universes of sACN input.
+ * Uses perAddressPriority if available for a given source and the universe priority if not.
+ * 
+ * @param {SACNUniverse[]} sources An array of the sources.
+ * 
+ * @returns {number[]} 
+ */
+var merge_peraddress = function merge_peraddress(sources) {
+  if (sources === undefined || sources === null || sources.length === 0)
+  {
+    return [];
+  }
+  return sources[0].values;  
+}
+
+
+/**
+ * Merges two universes of sACN input.
+ * Ignores perAddressPriority and just uses universe priority.
+ * 
+ * @param {SACNUniverse[]} sources An array of the sources.
+ * 
+ * @returns {number[]} 
+ */
+var merge_peruniverse = function merge_peruniverse(sources) {
+  if (sources === undefined || sources === null || sources.length === 0)
+  {
+    return [];
+  }
+  return sources[0].values;  
+}
+
+/**
+ * Merges two universes of sACN input.
+ * Uses per-address priority by default, but can be set to run in per-universe priority mode.
+ * (Note: If a universe does not have a per-address priority array, it will use the universe priority for all addresses.)
+ * 
+ * @param {SACNUniverse[]} sources An array of the sources.
+ * @param {bool} [peraddress] Whether or not to use 
+ * 
+ * @returns {number[]} The merged values of the universe.
+ */
+var merge = function merge(sources, peraddress) {
+  if (peraddress === undefined)
+  {
+    peraddress = true
+  }
+  if (peraddress)
+  {
+    return merge_peraddress(sources)
+  } 
+  else
+  {
+    return merge_peruniverse(sources)
+  }
+}
+
+/**
  * Example function that takes an array of objects and
  * returns an array.
  * @param {TestObj[]} input an array of input objects
@@ -25,5 +93,6 @@ var calculate = function calculate(input) {
 };
 
 module.exports = {
-  calculate: calculate
+  calculate: calculate,
+  merge: merge
 };
--- test/calculate.spec.js
+++ test/calculate.spec.js
@@ -1,6 +1,60 @@
 const calc = require("../src/calculate");
 const {assert} = require("chai");
 
+describe("Test Per-Address Priority methods", () =>{
+  it("Should return an empty array with no input", () => {
+    assert.deepEqual([], calc.merge([]));
+    assert.deepEqual([], calc.merge());
+  });
+  it("Should TEST", () => {
+    assert.deepEqual([1,2,3], calc.merge([{'values': [1,2,3], 'priority': 100}]));
+  });
+
+})
+
+describe("Test Per-Universe Priority methods", () => {
+
+  it("Should return an empty array with no input", () => {
+    assert.deepEqual([], calc.merge([], false));
+    assert.deepEqual([], calc.merge());
+  });
+  it ("Simple one universe tests.", () => {
+    assert.deepEqual([1,2,3], calc.merge([{'values':[1,2,3]}], false));
+    assert.deepEqual([1,2,3], calc.merge([{'values':[1,2,3], 'priority': 100}], false));
+    //assert.deepEqual([], calc.merge([{'values':[1,2,3], 'priority': 0}], false));
+    
+  });
+
+  it ("If there are multiple universes at the same priority, we do an HTP merge.", () => {
+    const universe_one = {'values': [1,2,3], 'priority': 100};
+    universe_two = {'values': [4,5,6], 'priority': 100};
+    const universe_three = {'values': [3, 2, 1], 'priority': 100};
+    assert.deepEqual([4, 5, 6], calc.merge([universe_one, universe_two, universe_three], false));
+    assert.deepEqual([3, 2, 3], calc.merge([universe_one, universe_three], false));
+    universe_two['priority'] = 50; 
+    assert.deepEqual([3, 2, 3], calc.merge([universe_one, universe_two, universe_three], false));
+  });
+
+  it ("We take the highest priority universe if there is one.", () => {
+    const universe_one = {'values': [1,2,3], 'priority': 100};
+    const universe_two = {'values': [4,5,6], 'priority': 99};
+    assert.deepEqual([1, 2, 3], calc.merge([universe_one, universe_two], false));
+  })
+
+  it ("If a universe has higher priority but less addresses filled, fill the latter ones from the next highest priority universe.", () => {
+    assert.deepEqual([1, 2, 3, 5],
+      calc.merge([{
+        'values': [1, 2, 3],
+        'priority': 100
+      }, 
+      {
+        'values': [101, 102, 103, 5],
+        'priority': 5
+      }], false));
+  })
+
+})
+
 describe("Example of testing a module", () => {
 
   it("Should return a number", () => {
